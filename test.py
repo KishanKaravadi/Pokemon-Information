f16fb59327bbacb2913e8f233e7ca29d43f166c6
Kishan Karavadi <kishan.karavadi@gmail.com>
2023-05-19 23:45:06 -0400

    type checking

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import pokebase as pb
 from pokebase import cache
 from pokebase.common import ENDPOINTS
 
-cache.set_cache('testing')
+# cache.set_cache('testing')
 
-hi = pb.api.get_data('berry', 1, force_lookup=True)
-print(hi)
+hi = pb.api.get_data('type', force_lookup=True)
+for key in hi['results']:
+    print(key)
