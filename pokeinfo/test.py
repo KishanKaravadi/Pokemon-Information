15156b3f1259699f6617d783a9ca4e0d3da7878b
Kishan Karavadi <kishan.karavadi@gmail.com>
2023-05-24 11:17:53 -0400

    works with the actual api (reverted validate)

--- pokeinfo/test.py
+++ pokeinfo/test.py
@@ -2,21 +2,23 @@
 import pokebase as pb
 
 from pokebase import cache
+from pokebase import interface
 
 cache.set_cache('testing')
 
-print(pb.pokemon('charmander', force_lookup=True))
 # print(pb.api.get_data('pokemon', resource_id='charmander', force_lookup=True))
 
 
 def getInfo(poke_name) -> dict:
+    poke_id = interface._convert_name_to_id('pokemon', poke_name)
     poke_type = {'bug': 1, 'dark': 1, 'dragon': 1, 'electric': 1, 'fairy': 1, 'fighting': 1, 'fire': 1, 'flying': 1,
                  'ghost': 1, 'grass': 1, 'ground': 1, 'ice': 1, 'normal': 1, 'poison': 1, 'psychic': 1, 'rock': 1, 'steel': 1, 'water': 1}
     poke = pb.api.get_data(
-        'pokemon', resource_id=poke_name, force_lookup=True)
+        'pokemon', resource_id=poke_id, force_lookup=True)
     for type in poke['types']:
         name = type['type']['name']
-        types = pb.api.get_data('type', resource_id=name, force_lookup=True)
+        type_id = interface._convert_name_to_id('type', name)
+        types = pb.api.get_data('type', resource_id=type_id, force_lookup=True)
         for category in ['double_damage_from']:
             for obj in types['damage_relations'][category]:
                 poke_type[obj['name']] *= 2
@@ -27,3 +29,6 @@ def getInfo(poke_name) -> dict:
             for obj in types['damage_relations'][category]:
                 poke_type[obj['name']] *= 0
     return poke_type
+
+
+print(getInfo('charmander'))
